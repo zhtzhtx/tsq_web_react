1c680ee625d1dc82b14e48af861959cc3cb5d524
hanting <790536079@qq.com>
2020-04-30 20:25:41 +0800

    Updates

---
+++ precache-manifest.40933ebae9e6d161b7d93a39f0b49c8b.js
@@ -0,0 +1,290 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf1acb907e4d0fcd72b62b7c11562167",
+    "url": "/tsq_web_react/index.html"
+  },
+  {
+    "revision": "9b1a77ab7310fb000872",
+    "url": "/tsq_web_react/static/css/2.3b3e3e42.chunk.css"
+  },
+  {
+    "revision": "44b060d734f176d46135",
+    "url": "/tsq_web_react/static/css/main.4fe935b5.chunk.css"
+  },
+  {
+    "revision": "9b1a77ab7310fb000872",
+    "url": "/tsq_web_react/static/js/2.f5a39d2b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/tsq_web_react/static/js/2.f5a39d2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44b060d734f176d46135",
+    "url": "/tsq_web_react/static/js/main.5b3ed856.chunk.js"
+  },
+  {
+    "revision": "91d691db5aef1fb5cf71",
+    "url": "/tsq_web_react/static/js/runtime-main.ec679594.js"
+  },
+  {
+    "revision": "7f5d93295119a0009b9c0312046f4fe3",
+    "url": "/tsq_web_react/static/media/111.7f5d9329.png"
+  },
+  {
+    "revision": "35b0115303528bcf4ed7ffb3629cb1f3",
+    "url": "/tsq_web_react/static/media/222.35b01153.png"
+  },
+  {
+    "revision": "be11ad8e2e6b01ab7af0f979e67b7528",
+    "url": "/tsq_web_react/static/media/333.be11ad8e.png"
+  },
+  {
+    "revision": "63b28293ec09a41092fa19b0a56a0c60",
+    "url": "/tsq_web_react/static/media/444.63b28293.png"
+  },
+  {
+    "revision": "fef8c12b03e37f6d2839edc01380d6c4",
+    "url": "/tsq_web_react/static/media/555.fef8c12b.png"
+  },
+  {
+    "revision": "03833b6aec51ccdc4d4b0321071467f3",
+    "url": "/tsq_web_react/static/media/666.03833b6a.png"
+  },
+  {
+    "revision": "104d819799119e0829f3cc2ad2e74daa",
+    "url": "/tsq_web_react/static/media/Sceniclandscape_title.104d8197.png"
+  },
+  {
+    "revision": "5d67a667362a26cac11cac14e0a52adb",
+    "url": "/tsq_web_react/static/media/bg1231231.5d67a667.png"
+  },
+  {
+    "revision": "40c32dadb79bdea1e222e249741c9406",
+    "url": "/tsq_web_react/static/media/comQuestion1.40c32dad.png"
+  },
+  {
+    "revision": "d0272e6012e87558b265c3c1b988cab1",
+    "url": "/tsq_web_react/static/media/comQuestion2.d0272e60.png"
+  },
+  {
+    "revision": "bb0de45e341f73abfaf62648febf2afc",
+    "url": "/tsq_web_react/static/media/comQuestion3.bb0de45e.png"
+  },
+  {
+    "revision": "05680dc0e9f5a55d48afa00e1dee4a83",
+    "url": "/tsq_web_react/static/media/comQuestion4.05680dc0.png"
+  },
+  {
+    "revision": "2d3802b123a2d418659aafcf33875a16",
+    "url": "/tsq_web_react/static/media/comQuestion5.2d3802b1.png"
+  },
+  {
+    "revision": "b4dcfe4e5e5c25a61eac2e3466b54076",
+    "url": "/tsq_web_react/static/media/comQuestion6.b4dcfe4e.png"
+  },
+  {
+    "revision": "f81c3cce2961b0b59a72871e01e508df",
+    "url": "/tsq_web_react/static/media/comQuestion7.f81c3cce.png"
+  },
+  {
+    "revision": "38748a5667e88009727582835c145df5",
+    "url": "/tsq_web_react/static/media/comQuestion8.38748a56.png"
+  },
+  {
+    "revision": "10ff0911e3dea6847c0549fc8e688c96",
+    "url": "/tsq_web_react/static/media/company_content1.10ff0911.png"
+  },
+  {
+    "revision": "8a2e620e188147a88b415e6c22eaa1e9",
+    "url": "/tsq_web_react/static/media/company_content2.8a2e620e.png"
+  },
+  {
+    "revision": "aa9002a59152eab8c6fecbb3e3ab9ce3",
+    "url": "/tsq_web_react/static/media/company_content3.aa9002a5.png"
+  },
+  {
+    "revision": "7d5e649c7faea6babf9c7f0f4a5a85ae",
+    "url": "/tsq_web_react/static/media/company_content4.7d5e649c.png"
+  },
+  {
+    "revision": "8b0e270d90522ea607dfea14fa8e2a48",
+    "url": "/tsq_web_react/static/media/company_content5.8b0e270d.png"
+  },
+  {
+    "revision": "5e80dbfb26a1ebe37eb68ff1582803fe",
+    "url": "/tsq_web_react/static/media/company_content6.5e80dbfb.png"
+  },
+  {
+    "revision": "d71e2a8d5bbf2c0d5d691704f1dc3865",
+    "url": "/tsq_web_react/static/media/company_content7.d71e2a8d.png"
+  },
+  {
+    "revision": "eb0f6a86c8a319af5160cba3e8d6949b",
+    "url": "/tsq_web_react/static/media/contentfengguang1.eb0f6a86.png"
+  },
+  {
+    "revision": "9f2ae038d47d1705aec6e61b170abdea",
+    "url": "/tsq_web_react/static/media/contentfengguang2.9f2ae038.png"
+  },
+  {
+    "revision": "845e9d7edcab4563a0a4c26ff1241dc8",
+    "url": "/tsq_web_react/static/media/contentfengguang3.845e9d7e.png"
+  },
+  {
+    "revision": "b27e1bca5ab4614e739841d6fe0f0f1e",
+    "url": "/tsq_web_react/static/media/contentfengguang4.b27e1bca.png"
+  },
+  {
+    "revision": "8b0fc067a059028e7115058853786597",
+    "url": "/tsq_web_react/static/media/contentfengguang5.8b0fc067.png"
+  },
+  {
+    "revision": "fe852cd45b413eeaa397cdef338fc456",
+    "url": "/tsq_web_react/static/media/contentfengguang6.fe852cd4.png"
+  },
+  {
+    "revision": "30baa5137c76695528f00ee304a028ce",
+    "url": "/tsq_web_react/static/media/fengguangditu.30baa513.png"
+  },
+  {
+    "revision": "b0b3582ce7f80586b41b9a46f31020aa",
+    "url": "/tsq_web_react/static/media/foot_wx_code.b0b3582c.png"
+  },
+  {
+    "revision": "7537fa47a57726b40283c0ecaaf36422",
+    "url": "/tsq_web_react/static/media/home_lunbo1.7537fa47.png"
+  },
+  {
+    "revision": "b57a0a6ec9292a194de1429948de1824",
+    "url": "/tsq_web_react/static/media/home_lunbo2.b57a0a6e.png"
+  },
+  {
+    "revision": "c9c7ef66788b18dd245fac7c50273853",
+    "url": "/tsq_web_react/static/media/home_lunbo3.c9c7ef66.png"
+  },
+  {
+    "revision": "b8985a56e9c87e16c6c922be5b19ffd5",
+    "url": "/tsq_web_react/static/media/home_scenery_spot1.b8985a56.png"
+  },
+  {
+    "revision": "0b5cf933c1ae285a0cfbbfa5366ee9c5",
+    "url": "/tsq_web_react/static/media/home_scenery_spot2.0b5cf933.png"
+  },
+  {
+    "revision": "437b4fd26701da14ea3c10d1ba9be1f5",
+    "url": "/tsq_web_react/static/media/home_scenery_spot3.437b4fd2.png"
+  },
+  {
+    "revision": "86b6ee5f1810c67ff9450b6314989712",
+    "url": "/tsq_web_react/static/media/home_scenic_about.86b6ee5f.png"
+  },
+  {
+    "revision": "48882bcfda2945439d43ae1808aab5dc",
+    "url": "/tsq_web_react/static/media/home_video.48882bcf.png"
+  },
+  {
+    "revision": "ec25685c4da201cf356987f08c1fe92a",
+    "url": "/tsq_web_react/static/media/lunbo1.ec25685c.jpg"
+  },
+  {
+    "revision": "ba1e5ac49d251653db56a56797e16138",
+    "url": "/tsq_web_react/static/media/lunbo2.ba1e5ac4.png"
+  },
+  {
+    "revision": "25dac80517a4eba973f804b3919403f3",
+    "url": "/tsq_web_react/static/media/lunbo3.25dac805.png"
+  },
+  {
+    "revision": "fdef9db89ef6091fb227be2874fa9b5a",
+    "url": "/tsq_web_react/static/media/lunbo4.fdef9db8.png"
+  },
+  {
+    "revision": "eea6931c62c9c26ff3ddbee80103b1e8",
+    "url": "/tsq_web_react/static/media/lunbo5.eea6931c.jpg"
+  },
+  {
+    "revision": "fbb9e99f420769ece2aaf13517430834",
+    "url": "/tsq_web_react/static/media/markers.fbb9e99f.png"
+  },
+  {
+    "revision": "967db283fc1253a36e8ec2c9494c8ac4",
+    "url": "/tsq_web_react/static/media/scenicGuide_main_guidemap1.967db283.png"
+  },
+  {
+    "revision": "2bd9d9f51f8a4f50c6bc9098014a91b5",
+    "url": "/tsq_web_react/static/media/scenicGuide_main_guidemap2.2bd9d9f5.png"
+  },
+  {
+    "revision": "8988eca3664b756e49d7045108b2f9ea",
+    "url": "/tsq_web_react/static/media/scenicGuide_main_guidemap3.8988eca3.png"
+  },
+  {
+    "revision": "7d271561ce1956a11730c28d17c2c34f",
+    "url": "/tsq_web_react/static/media/scenicGuide_main_guidemap4.7d271561.png"
+  },
+  {
+    "revision": "8d3807a9faa3b1b17bfdb4327f3bf2f2",
+    "url": "/tsq_web_react/static/media/scenicGuide_main_guidemap5.8d3807a9.png"
+  },
+  {
+    "revision": "9f48805cdf2443448fc50c7515586274",
+    "url": "/tsq_web_react/static/media/scenicGuide_main_guidemap6.9f48805c.png"
+  },
+  {
+    "revision": "fc134f6e473630c43e91472e485e76e7",
+    "url": "/tsq_web_react/static/media/scenicGuide_main_guidemap7.fc134f6e.png"
+  },
+  {
+    "revision": "8c7a4e09e0b9e52350596e749459526d",
+    "url": "/tsq_web_react/static/media/scenicGuide_main_guidemap8.8c7a4e09.png"
+  },
+  {
+    "revision": "4b47c963dc33347ea1db875d17843b98",
+    "url": "/tsq_web_react/static/media/scenicGuide_ticket_position.4b47c963.png"
+  },
+  {
+    "revision": "534b23d74d989e5f6b4ef6367225e1d7",
+    "url": "/tsq_web_react/static/media/scenicGuide_traffic_bus_you2.534b23d7.png"
+  },
+  {
+    "revision": "ae4dc9af90ae3cd46354d213a05c339f",
+    "url": "/tsq_web_react/static/media/scenicGuide_traffic_bus_you3.ae4dc9af.png"
+  },
+  {
+    "revision": "9576b596de422d64bfed2592fd561e4f",
+    "url": "/tsq_web_react/static/media/scenicIntroduct_img1.9576b596.png"
+  },
+  {
+    "revision": "64047f85e19ea76323045b391ca8843d",
+    "url": "/tsq_web_react/static/media/scenicIntroduct_img2.64047f85.png"
+  },
+  {
+    "revision": "4369fd8973bd978f1656293dc485dcfa",
+    "url": "/tsq_web_react/static/media/scenicIntroduct_img3.4369fd89.png"
+  },
+  {
+    "revision": "5003bf80c67bd1b0be3878232f7a1958",
+    "url": "/tsq_web_react/static/media/scenicIntroduct_img4.5003bf80.png"
+  },
+  {
+    "revision": "0b4eaf9f7bc8de6e333054ea331f67b1",
+    "url": "/tsq_web_react/static/media/scenicIntroduct_img5.0b4eaf9f.png"
+  },
+  {
+    "revision": "3c3eb635d727d1969ce42ce4a7729a8e",
+    "url": "/tsq_web_react/static/media/traffic_routes_bus.3c3eb635.png"
+  },
+  {
+    "revision": "0077bc8af97d06a2b0486c28da88c815",
+    "url": "/tsq_web_react/static/media/traffic_routes_railway.0077bc8a.png"
+  },
+  {
+    "revision": "a5bb39b24acb02d056859af22d9081f8",
+    "url": "/tsq_web_react/static/media/traffic_routes_self_bus.a5bb39b2.png"
+  },
+  {
+    "revision": "20fd2d217aea1cd83afe5a3214063956",
+    "url": "/tsq_web_react/static/media/traffic_routes_travel_bus.20fd2d21.png"
+  }
+]);
